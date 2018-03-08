5ad4b5feafc307f408bbb57273ef9a6042182c9f
Herles Incalla <herles.incalla@gmail.com>
2018-03-08 15:06:44 -0500

    Init commit

---
+++ __manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Helper",
+
+    'summary': """Condominium APP""",
+
+    'description': """
+        Odoo Theme:
+            - herles.incalla@gmail.com
+            - yojana.vilca@gmail.com
+    """,
+
+    'author': "Aranda Software Company",
+    'website': "http://www.arandasf.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Theme',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+    'js': [],
+    # always loaded
+    'data': [
+        'views/assets.xml',
+    ],
+    'qweb': ['static/src/xml/helper-templates.xml'],
+}
